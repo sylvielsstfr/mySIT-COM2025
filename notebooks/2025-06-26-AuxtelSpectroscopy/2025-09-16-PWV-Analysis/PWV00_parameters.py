453c700e44f7cd6b625922f548e73e25b12c3f0d
sylvielsstfr <dagoret@lal.in2p3.fr>
2025-09-18 21:34:05 +0200

    add OZ00_parameters.py and PWV00_parameters.py

---
+++ notebooks/2025-06-26-AuxtelSpectroscopy/2025-09-16-PWV-Analysis/PWV00_parameters.py
@@ -0,0 +1,34 @@
+# Selection parameters for cuts
+DCCDMINFIG = 185.0;
+DCCDMAXFIG = 190.0;
+DCCDMINCUT = 186.7;
+DCCDMAXCUT = 188.0;
+CHI2CUT = 300.;
+EXPTIMECUT = 20.0;    
+PWVMINCUT = 0.;
+PWVMAXCUT = 20.;
+OZMINCUT = 0.;
+OZMAXCUT = 650.0;    
+
+def getSelectionCutOld(df_spec, chi2max=20., pwvmin=0.1, pwvmax = 14.9):
+    cut =  (df_spec["CHI2_FIT"]<chi2max) & (df_spec["PWV [mm]_x"] > pwvmin) & (df_spec["PWV [mm]_x"] < pwvmax) 
+     #(df_spec["ozone [db]_y"] > ozmin) & (df_spec["ozone [db]_y"] < ozmax) & (df_spec["TARGET"] == "HD185975")
+    return cut
+
+    
+def getSelectionCut(df_spec, chi2max=CHI2CUT, pwvmin=PWVMINCUT, pwvmax = PWVMAXCUT,ozmin=OZMINCUT,ozmax=OZMAXCUT):
+    cut =  (df_spec["CHI2_FIT"]<chi2max) & (df_spec["PWV [mm]_x"] > pwvmin) & (df_spec["PWV [mm]_x"] < pwvmax) & (df_spec["D2CCD"]>DCCDMINCUT) &  (df_spec["D2CCD"]<DCCDMAXCUT) & \
+    (df_spec['EXPTIME'] > EXPTIMECUT ) 
+    return cut 
+
+def getSelectionCutNoPolar(df_spec, chi2max=CHI2CUT, pwvmin=PWVMINCUT, pwvmax=PWVMAXCUT,ozmin=OZMINCUT,ozmax=OZMAXCUT):
+    cut =  (df_spec["CHI2_FIT"]<chi2max) & (df_spec["PWV [mm]_x"] > pwvmin) & (df_spec["PWV [mm]_x"] < pwvmax) & (df_spec["D2CCD"]>DCCDMINCUT) &  (df_spec["D2CCD"]<DCCDMAXCUT) & \
+    (df_spec['EXPTIME'] > EXPTIMECUT ) &   (df_spec["TARGET"] != "HD185975")
+    return cut
+
+def getSelectionCutWithPolar(df_spec, chi2max=CHI2CUT, pwvmin=PWVMINCUT, pwvmax=PWVMAXCUT,ozmin=OZMINCUT,ozmax=OZMAXCUT):
+    cut =  (df_spec["CHI2_FIT"]<chi2max) & (df_spec["PWV [mm]_x"] > pwvmin) & (df_spec["PWV [mm]_x"] < pwvmax) & (df_spec["D2CCD"]>DCCDMINCUT) &  (df_spec["D2CCD"]<DCCDMAXCUT) & \
+    (df_spec['EXPTIME'] > EXPTIMECUT ) &   (df_spec["TARGET"] == "HD185975")
+    return cut
+
+    
