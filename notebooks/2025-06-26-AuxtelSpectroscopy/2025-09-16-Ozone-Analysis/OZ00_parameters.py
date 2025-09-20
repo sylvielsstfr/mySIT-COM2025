70fafe167e0d0991f0b88b95f7c9d6f9c45d2136
Sylvie Dagoret-Campagne <sylvie.dagoret-campagne@ijclab.in2p3.fr>
2025-09-20 06:16:50 -0700

    improve ozon

--- notebooks/2025-06-26-AuxtelSpectroscopy/2025-09-16-Ozone-Analysis/OZ00_parameters.py
+++ notebooks/2025-06-26-AuxtelSpectroscopy/2025-09-16-Ozone-Analysis/OZ00_parameters.py
@@ -44,3 +44,8 @@ def getSelectionCutWithPolar(df_spec, chi2max=CHI2CUT, pwvmin=PWVMINCUT, pwvmax=
     return cut
 
     
+
+
+# Merra2 pathname
+
+filename_m2 = "../2025-09-16-SpectroMerra2/MerradataMerged/Merge_inst1_2d_asm_Nx_M2I1NXASM-2021-2025.csv"
