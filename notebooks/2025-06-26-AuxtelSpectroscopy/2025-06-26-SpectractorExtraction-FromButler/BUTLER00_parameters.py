2caa13d0dbd391811cebe4f312b84f04c49d22a1
Sylvie Dagoret-Campagne <sylvie.dagoret-campagne@ijclab.in2p3.fr>
2025-09-22 05:31:20 -0700

    add data for run_v6:u/dagoret/auxtel_run_20250921_w_2025_38_spectractorv32_main_gains_holoallfilt_a

--- notebooks/2025-06-26-AuxtelSpectroscopy/2025-06-26-SpectractorExtraction-FromButler/BUTLER00_parameters.py
+++ notebooks/2025-06-26-AuxtelSpectroscopy/2025-06-26-SpectractorExtraction-FromButler/BUTLER00_parameters.py
@@ -1,9 +1,14 @@
 # input file configuration
 
+# Select run version tag to be used in EXTR_viewSpectractor notebooks 
+version_run = "run_v6"
+# Configuration for the butler repo associated to the version_run
+FLAG_REPO_EMBARGO = False
 
-version_run = "run_v5"
-legendtag = {"run_v1" : "v3.1.0","run_v2":"v3.1.0","run_v3":"v3.2.0", "run_v4":"v3.2.0","run_v5":"v3.2.0"}
+# Associate the tag to the Spectractor runparameters (to be used in plots)
+legendtag = {"run_v1" : "v3.1.0 (/repo/main, w_2025_25,empty,gain)","run_v2":"v3.1.0 (/repo/main, w_2025_25,all-filts,gain)","run_v3":"v3.2.0 (/repo/embargo, w_2025_36,gain),", "run_v4":"v3.2.0 (/repo/embargo,w_2025_36,gain)","run_v5":"v3.2.0  (/repo/embargo,w_2025_36,ptc)","run_v6":"v3.2.0  (/repo/embargo,w_2025_38,gain)"}
 
+# List of user collection in butler  where the results of spectractor run are
 butlerusercollectiondict = {
     # /repo/main
     "run_v1":"u/dagoret/auxtel_run_20250912a",
@@ -15,10 +20,12 @@ butlerusercollectiondict = {
     "run_v4":"u/dagoret/auxtel_run_20250920_w_2025_36_spectractorv32_embargo_gains_holoallfilt_b",
     # /repo/embargo, ptc 
     "run_v5":"u/dagoret/auxtel_run_20250921_w_2025_36_spectractorv32_embargo_ptc_holoallfilt_a",
+    # /repo/main, gains
+    "run_v6":"u/dagoret/auxtel_run_20250921_w_2025_38_spectractorv32_main_gains_holoallfilt_a",
 }
 
 
-
+# path of output files Spectractor parameters Extracted from Butler 
 extractedfilesdict = {
     # /repo/main
     "run_v1": "../2025-06-26-SpectractorExtraction-FromButler/data/spectro/auxtel_atmosphere_20250912a_repomain_v1.npy",
@@ -26,4 +33,26 @@ extractedfilesdict = {
     # /repo/embargo, gain
     "run_v4":"../2025-06-26-SpectractorExtraction-FromButler/data/spectro/auxtel_run_u_dagoret_auxtel_run_20250920_w_2025_36_spectractorv32_embargo_gains_holoallfilt_b_v1.npy",
     "run_v5":"../2025-06-26-SpectractorExtraction-FromButler/data/spectro/auxtel_run_u_dagoret_auxtel_run_20250921_w_2025_36_spectractorv32_embargo_ptc_holoallfilt_a_v1.npy",
+    # /repo/main
+    "run_v6":"../2025-06-26-SpectractorExtraction-FromButler/data/spectro/auxtel_run_u_dagoret_auxtel_run_20250921_w_2025_38_spectractorv32_main_gains_holoallfilt_a_v1.npy",
 }
+
+
+def DumpConfig():
+    """
+    Dump the configuration chosen here
+    """
+    print("**************************************************************************")
+    print("*                         DumpConfig()                                   *")
+    print(f" \t - version_run = {version_run}")
+    if FLAG_REPO_EMBARGO:
+        print(f" \t - butler repo : /repo/embargo")
+    else:
+        print(f" \t - butler repo : /repo/main")
+    print(f" \t annotation tag = {legendtag[version_run]}")
+    print(f" \t butler_collection = {butlerusercollectiondict[version_run]}")
+    print(f" \t extracted file from butler collection = {extractedfilesdict[version_run]}")
+    print("**************************************************************************")
+    
+    
+          
---
+++ notebooks/2025-06-26-AuxtelSpectroscopy/README.md
@@ -0,0 +1,21 @@
+# README.md
+
+- author : Sylvie Dagoret-Campagne
+- affiliation : IJCLab/IN2P3/CNRS
+- creation date : 2025-09-22
+
+
+
+# Folder to handle butler and files 
+- 2025-06-26-SpectractorExtraction-FromButler  
+
+# Folder to handle Precipitable water vapor analysis
+-2025-09-16-PWV-Analysis 
+
+
+# Folder to handle Ozone Analysis
+- 2025-09-16-Ozone-Analysis	
+
+
+# Folder to handle MERRA2 data and simulation with getObsAtmo
+- 2025-09-16-SpectroMerra
