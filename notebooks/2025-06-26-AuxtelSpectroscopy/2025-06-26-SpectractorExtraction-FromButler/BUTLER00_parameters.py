634319968428f3f65fe45ac9e227048e94dfec12
Sylvie Dagoret-Campagne <sylvie.dagoret-campagne@ijclab.in2p3.fr>
2025-09-22 07:06:55 -0700

    update PWV01_ExploreHoloQualityPWV.ipynb after run_v4, run_v5 on 2025 data

--- notebooks/2025-06-26-AuxtelSpectroscopy/2025-06-26-SpectractorExtraction-FromButler/BUTLER00_parameters.py
+++ notebooks/2025-06-26-AuxtelSpectroscopy/2025-06-26-SpectractorExtraction-FromButler/BUTLER00_parameters.py
@@ -6,7 +6,7 @@ version_run = "run_v6"
 FLAG_REPO_EMBARGO = False
 
 # Associate the tag to the Spectractor runparameters (to be used in plots)
-legendtag = {"run_v1" : "v3.1.0 (/repo/main, w_2025_25,empty,gain)","run_v2":"v3.1.0 (/repo/main, w_2025_25,all-filts,gain)","run_v3":"v3.2.0 (/repo/embargo, w_2025_36,gain),", "run_v4":"v3.2.0 (/repo/embargo,w_2025_36,gain)","run_v5":"v3.2.0  (/repo/embargo,w_2025_36,ptc)","run_v6":"v3.2.0  (/repo/embargo,w_2025_38,gain)"}
+legendtag = {"run_v1" : "v3.1.0 (/repo/main, w_2025_25,empty,gain)","run_v2":"v3.1.0 (/repo/main, w_2025_25,all-filts,gain)","run_v3":"v3.2.0 (/repo/embargo, w_2025_36,gain),", "run_v4":"v3.2.0 (/repo/embargo,w_2025_36,gain)","run_v5":"v3.2.0  (/repo/embargo,w_2025_36,ptc)","run_v6":"v3.2.0  (/repo/main,w_2025_38,gain)"}
 
 # List of user collection in butler  where the results of spectractor run are
 butlerusercollectiondict = {
@@ -38,6 +38,18 @@ extractedfilesdict = {
 }
 
 
+# path of output files Spectractor parameters Extracted from Butler and merged with exposure list from butler registry
+mergedextractedfilesdict = {
+    # /repo/main
+    "run_v2": "../2025-06-26-SpectractorExtraction-FromButler/data/spectro_merged/auxtel_run_20250917_w_2025_25_spectractorv31_holoallfilt_a_repomain_v1_merged.npy",
+    # /repo/embargo, gain
+    "run_v4":"../2025-06-26-SpectractorExtraction-FromButler/data/spectro_merged/auxtel_run_u_dagoret_auxtel_run_20250920_w_2025_36_spectractorv32_embargo_gains_holoallfilt_b_v1_merged.npy",
+    "run_v5":"../2025-06-26-SpectractorExtraction-FromButler/data/spectro_merged/auxtel_run_u_dagoret_auxtel_run_20250921_w_2025_36_spectractorv32_embargo_ptc_holoallfilt_a_v1_merged.npy",
+    # /repo/main
+    "run_v6":"../2025-06-26-SpectractorExtraction-FromButler/data/spectro_merged/auxtel_run_u_dagoret_auxtel_run_20250921_w_2025_38_spectractorv32_main_gains_holoallfilt_a_v1_merged.npy",
+}
+
+
 def DumpConfig():
     """
     Dump the configuration chosen here
@@ -49,9 +61,10 @@ def DumpConfig():
         print(f" \t - butler repo : /repo/embargo")
     else:
         print(f" \t - butler repo : /repo/main")
-    print(f" \t annotation tag = {legendtag[version_run]}")
-    print(f" \t butler_collection = {butlerusercollectiondict[version_run]}")
-    print(f" \t extracted file from butler collection = {extractedfilesdict[version_run]}")
+    print(f" \t - annotation tag = {legendtag[version_run]}")
+    print(f" \t - butler_collection = {butlerusercollectiondict[version_run]}")
+    print(f" \t - extracted file from butler collection = {extractedfilesdict[version_run]}")
+    print(f" \t - extracted and merged file from butler collection = {mergedextractedfilesdict[version_run]}")
     print("**************************************************************************")
     
     
--- notebooks/2025-06-26-AuxtelSpectroscopy/2025-09-16-PWV-Analysis/PWV00_parameters.py
+++ notebooks/2025-06-26-AuxtelSpectroscopy/2025-09-16-PWV-Analysis/PWV00_parameters.py
@@ -1,16 +1,80 @@
 # input file configuration
 
 
-version_results = "v3"
-legendtag = {"v1" : "v3.1.0","v2":"v3.1.0", "v3":"v3.1.0"}
+version_results_old = "v3"
+legendtag_old = {"v1" : "v3.1.0","v2":"v3.1.0", "v3":"v3.1.0"}
 
-atmfilenamesdict = {"v1":"../2025-06-26-SpectractorExtraction-FromButler/data/spectro/auxtel_atmosphere_20250912a_repomain_v1.npy",
+atmfilenamesdict_old = {"v1":"../2025-06-26-SpectractorExtraction-FromButler/data/spectro/auxtel_atmosphere_20250912a_repomain_v1.npy",
                     "v2":"../2025-06-26-SpectractorExtraction-FromButler/data/spectro/auxtel_run_20250917_w_2025_25_spectractorv31_holoallfilt_a_repomain_v1.npy",
-                   "v3":"../2025-06-26-SpectractorExtraction-FromButler/data/spectro_merged/auxtel_run_20250917_w_2025_25_spectractorv31_holoallfilt_a_repomain_v1_merged.npy",}
+                    "v3":"../2025-06-26-SpectractorExtraction-FromButler/data/spectro_merged/auxtel_run_20250917_w_2025_25_spectractorv31_holoallfilt_a_repomain_v1_merged.npy",}
+
+# input file configuration
+
+# Select run version tag to be used in EXTR_viewSpectractor notebooks 
+version_run = "run_v5"
+# Configuration for the butler repo associated to the version_run
+FLAG_REPO_EMBARGO = False
+
+# Associate the tag to the Spectractor runparameters (to be used in plots)
+legendtag = {"run_v1" : "v3.1.0 (/repo/main, w_2025_25,empty,gain)","run_v2":"v3.1.0 (/repo/main, w_2025_25,all-filts,gain)","run_v3":"v3.2.0 (/repo/embargo, w_2025_36,gain),", "run_v4":"v3.2.0 (/repo/embargo,w_2025_36,gain)","run_v5":"v3.2.0  (/repo/embargo,w_2025_36,ptc)","run_v6":"v3.2.0  (/repo/main,w_2025_38,gain)"}
+
+# List of user collection in butler  where the results of spectractor run are
+butlerusercollectiondict = {
+    # /repo/main
+    "run_v1":"u/dagoret/auxtel_run_20250912a",
+    # /repo/main
+    "run_v2":"u/dagoret/auxtel_run_20250917_w_2025_25_spectractorv31_holoallfilt_a",
+    # /repo/embargo
+    "run_v3":"u/dagoret/auxtel_run_20250920_w_2025_36_spectractorv32_embargo_gains_holoallfilt_a",
+    # /repo/embargo, gains
+    "run_v4":"u/dagoret/auxtel_run_20250920_w_2025_36_spectractorv32_embargo_gains_holoallfilt_b",
+    # /repo/embargo, ptc 
+    "run_v5":"u/dagoret/auxtel_run_20250921_w_2025_36_spectractorv32_embargo_ptc_holoallfilt_a",
+    # /repo/main, gains
+    "run_v6":"u/dagoret/auxtel_run_20250921_w_2025_38_spectractorv32_main_gains_holoallfilt_a",
+}
 
 
-atmfilename = atmfilenamesdict[version_results]
-tag = legendtag[version_results] 
+# path of output files Spectractor parameters Extracted from Butler 
+extractedfilesdict = {
+    # /repo/main
+    "run_v1": "../2025-06-26-SpectractorExtraction-FromButler/data/spectro/auxtel_atmosphere_20250912a_repomain_v1.npy",
+    "run_v2": "../2025-06-26-SpectractorExtraction-FromButler/data/spectro/auxtel_run_20250917_w_2025_25_spectractorv31_holoallfilt_a_repomain_v1.npy",
+    # /repo/embargo, gain
+    "run_v4":"../2025-06-26-SpectractorExtraction-FromButler/data/spectro/auxtel_run_u_dagoret_auxtel_run_20250920_w_2025_36_spectractorv32_embargo_gains_holoallfilt_b_v1.npy",
+    "run_v5":"../2025-06-26-SpectractorExtraction-FromButler/data/spectro/auxtel_run_u_dagoret_auxtel_run_20250921_w_2025_36_spectractorv32_embargo_ptc_holoallfilt_a_v1.npy",
+    # /repo/main
+    "run_v6":"../2025-06-26-SpectractorExtraction-FromButler/data/spectro/auxtel_run_u_dagoret_auxtel_run_20250921_w_2025_38_spectractorv32_main_gains_holoallfilt_a_v1.npy",
+}
+
+
+# path of output files Spectractor parameters Extracted from Butler and merged with exposure list
+mergedextractedfilesdict = {
+    # /repo/main
+    "run_v2": "../2025-06-26-SpectractorExtraction-FromButler/data/spectro_merged/auxtel_run_20250917_w_2025_25_spectractorv31_holoallfilt_a_repomain_v1_merged.npy",
+    # /repo/embargo, gain
+    "run_v4":"../2025-06-26-SpectractorExtraction-FromButler/data/spectro_merged/auxtel_run_u_dagoret_auxtel_run_20250920_w_2025_36_spectractorv32_embargo_gains_holoallfilt_b_v1_merged.npy",
+    "run_v5":"../2025-06-26-SpectractorExtraction-FromButler/data/spectro_merged/auxtel_run_u_dagoret_auxtel_run_20250921_w_2025_36_spectractorv32_embargo_ptc_holoallfilt_a_v1_merged.npy",
+    # /repo/main
+    "run_v6":"../2025-06-26-SpectractorExtraction-FromButler/data/spectro_merged/auxtel_run_u_dagoret_auxtel_run_20250921_w_2025_38_spectractorv32_main_gains_holoallfilt_a_v1_merged.npy",
+}
+
+
+atmfilename = mergedextractedfilesdict[version_run]
+tag = legendtag[version_run] 
+
+
+def DumpConfig():
+    """
+    Dump the configuration chosen here
+    """
+    print("**************************************************************************")
+    print("*                         DumpConfig()                                   *")
+    print(f" \t - version_run = {version_run}")
+    print(f" \t - annotation tag = {tag}")
+    print(f" \t - extracted file from butler collection = {atmfilename}")
+    print("**************************************************************************")
+    
 
 
 # Selection parameters for cuts
