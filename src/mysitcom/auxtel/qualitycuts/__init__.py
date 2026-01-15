7484ddf74d4ce1329ac606c0c1184697b4f5c02c
sylvielsstfr <dagoret@lal.in2p3.fr>
2026-01-16 00:18:41 +0100

    add histograms

--- src/mysitcom/auxtel/qualitycuts/__init__.py
+++ src/mysitcom/auxtel/qualitycuts/__init__.py
@@ -1,4 +1,8 @@
-from .libPlotsQCUT import get_filter_color,scatter_datetime,strip_datetime,bar_counts_by_night,plot_dccd_chi2_vs_time,plot_dccd_chi2_vs_time_by_filter,stripplot_target_vs_time,plot_dccd_chi2_vs_time_by_target_filter,plot_dccd_chi2_vs_time_by_target_filter_colorsedtype,summarize_dccd_chi2
+from .libPlotsQCUT import get_filter_color,scatter_datetime,strip_datetime,\
+bar_counts_by_night,plot_dccd_chi2_vs_time,plot_dccd_chi2_vs_time_by_filter,\
+stripplot_target_vs_time,plot_dccd_chi2_vs_time_by_target_filter,\
+plot_dccd_chi2_histo_by_target_filter,\
+plot_dccd_chi2_vs_time_by_target_filter_colorsedtype,summarize_dccd_chi2
 __all__ = ["get_filter_color",
            "scatter_datetime",
            "strip_datetime",
@@ -7,5 +11,6 @@ __all__ = ["get_filter_color",
            "plot_dccd_chi2_vs_time_by_filter",
            "stripplot_target_vs_time",
            "plot_dccd_chi2_vs_time_by_target_filter",
+           "plot_dccd_chi2_histo_by_target_filter",
            "plot_dccd_chi2_vs_time_by_target_filter_colorsedtype",
            "summarize_dccd_chi2"]
