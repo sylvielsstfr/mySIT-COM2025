8160076510128fcbc944d49f893ab8ee8e25fb5c
sylvielsstfr <dagoret@lal.in2p3.fr>
2026-01-23 23:33:15 +0100

    try run_v12

--- notebooks/2025-06-26-AuxtelSpectroscopy/2026-01-22-PWV-Analysis/PWV00_parameters.py
+++ notebooks/2025-06-26-AuxtelSpectroscopy/2026-01-22-PWV-Analysis/PWV00_parameters.py
@@ -17,6 +17,7 @@ import astropy.units as u
 #version_run = "run_v7"
 #version_run = "run_v11" # before v12
 version_run = "run2026_v01" # data 2025
+#version_run = "run_v12"
 # Configuration for the butler repo associated to the version_run
 
 map_run_butler_embargo = { 
